76d4db4920795ce9e2880c9a8cd6c214d3734054
Henrik Johansson <k.henrik.johansson@gmail.com>
2023-10-12 14:34:47 +0200

    Ny tutorial

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import Home from "./src/Screens/Components/Home";
-//hej
+
 const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
@@ -11,4 +11,3 @@ export default function App() {
 		</NavigationContainer>
 	);
 }
-
