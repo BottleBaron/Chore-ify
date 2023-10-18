87c1bf22d67adf9df19061ffff6f2245bfdf108e
Henrik Johansson <k.henrik.johansson@gmail.com>
2023-10-18 15:14:39 +0200

    draft

--- src/screens/mocked-screens/HomeScreen.tsx
+++ src/screens/mocked-screens/HomeScreen.tsx
@@ -2,18 +2,63 @@
 /* eslint-disable import/no-cycle */
 import * as React from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Text, Button, TouchableRipple } from 'react-native-paper';
+import { Text, Button, TouchableRipple, Divider } from 'react-native-paper';
 import { useAppTheme } from '../../contexts/ThemeContext';
 import { RootStackScreenProps } from '../../navigators/types';
+import { mockHouseholds } from '../../../assets/Data/MockData';
+
 
 type Props = RootStackScreenProps<'Home'>;
 
 export default function HomeScreen({ navigation }: Props) {
+  const mockedHousehold = mockHouseholds;
   const theme = useAppTheme();
 
   return (
     <View style={styles.container}>
-      <HouseHoldButton text="min knapp" />
+      <View style={{ alignItems: 'center', marginTop: -100 }}>
+        <Text style={{ color: theme.colors.color }}>DINA HUSHÅLL</Text>
+        <Divider
+          style={{
+            backgroundColor: theme.colors.color,
+            minWidth: '100%',
+            height: 3,
+          }}
+        />
+      </View>
+      <TouchableRipple
+        style={styles.touchableRipple}
+        onPress={() => navigation.navigate('HouseholdDashboard')}
+      >
+        <View>
+          <Text style={{ color: theme.colors.color }}>
+            Familjen Johanssons hushåll
+          </Text>
+          <Text>🐙 </Text>
+        </View>
+      </TouchableRipple>
+
+      <View style={styles.bottomButtons}>
+        <Button
+          labelStyle={{ fontSize: 20 }}
+          icon="plus-circle-outline"
+          mode="contained"
+          style={styles.button}
+          contentStyle={styles.buttonContent}
+        >
+          {' '}
+          GÅ MED I ETT HUSHÅLL
+        </Button>
+        <Button
+          icon="plus-circle-outline"
+          mode="contained"
+          style={styles.button}
+          contentStyle={styles.buttonContent}
+        >
+          {' '}
+          SKAPA ETT NYTT HUSHÅLL
+        </Button>
+      </View>
     </View>
   );
 }
@@ -22,7 +67,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
 
   householddivider: {
@@ -34,31 +79,34 @@ const styles = StyleSheet.create({
   hairline: {
     backgroundColor: '#A2A2A2',
     height: 2,
-    width: 165,
+    minWidth: '100%',
   },
-});
-
-interface HouseHoldButtonProps {
-  text: string;
-}
-
-export const HouseHoldButton: React.FC<HouseHoldButtonProps> = ({ text }) => (
-  <TouchableRipple theme={useAppTheme()}>
-    <View style={styles2.button}>
-      <Text style={styles2.buttonText}>{text}</Text>
-    </View>
-  </TouchableRipple>
-);
-const styles2 = StyleSheet.create({
+  touchableRipple: {
+    borderWidth: 1,
+    padding: 10, // Anpassa padding efter behov
+    borderRadius: 5, // Anpassa gränsvärdet efter behov
+  },
+  content: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomButtons: {
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
   button: {
-    borderRadius: 1,
-    paddingVertical: 14,
-    paddingHorizontal: 10,
+    width: '80%',
+    marginTop: 10,
+    borderRadius: 15,
+    minHeight: '10%',
   },
-  buttonText: {
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    fontSize: 16,
-    textAlign: 'center',
+  buttonContent: {
+    width: '100%',
+    flexDirection: 'row',
+  },
+  heading: {
+    marginTop: -100, // Justera avståndet till toppen
   },
 });
