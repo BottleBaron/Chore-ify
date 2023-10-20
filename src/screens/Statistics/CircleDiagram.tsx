4492fe6f5154d11ce150b6d199abcc84ad024945
m4d0x <robert.oxhjarta@hotmail.com>
2023-10-20 15:03:09 +0200

    Circlediagram supposed to be tested in Stats~

--- src/screens/Statistics/CircleDiagram.tsx
+++ src/screens/Statistics/CircleDiagram.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Dimensions } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
 import { PieChart } from 'react-native-chart-kit';
 
 const screenWidth = Dimensions.get('window').width;
 
 interface DataPoint {
-  name: string; // This can be the user's name
+  name: string;
   population: number;
   color: string;
   legendFontColor: string;
   legendFontSize: number;
-  avatar: string; // This can be the user's avatar emoji
+  avatar: string;
 }
 
 interface CircleDiagramProps {
@@ -18,20 +18,72 @@ interface CircleDiagramProps {
 }
 
 export default function CircleDiagram({ data }: CircleDiagramProps) {
+  const chartRadius = 110;
+  const totalPopulation = data.reduce(
+    (acc, point) => acc + point.population,
+    0,
+  );
+  let startAngle = 0;
+
+  const AvatarOverlays = data.map((point) => {
+    const percentage = point.population / totalPopulation;
+    const angle = percentage * 360;
+    const endAngle = startAngle + angle;
+    const midAngle = (startAngle + endAngle) / 2;
+
+    // Trigonometry to find x and y coordinates
+    const x = chartRadius * Math.cos((midAngle * Math.PI) / 180);
+    const y = chartRadius * Math.sin((midAngle * Math.PI) / 180);
+
+    startAngle = endAngle;
+
+    return (
+      <View
+        key={point.name}
+        style={[
+          styles.avatarOverlay,
+          {
+            left: chartRadius + x - 10,
+            top: chartRadius + y - 10,
+            backgroundColor: point.color,
+          },
+        ]}
+      >
+        <Text style={styles.avatarText}>{point.avatar}</Text>
+      </View>
+    );
+  });
+
   return (
-    <PieChart
-      data={data}
-      width={screenWidth}
-      height={220}
-      chartConfig={
-        {
-          // your chart configs here
-        }
-      }
-      accessor="population"
-      backgroundColor="transparent"
-      paddingLeft="15"
-      absolute
-    />
+    <View style={styles.container}>
+      <PieChart
+        data={data}
+        width={screenWidth}
+        height={220}
+        chartConfig={{}}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute
+      />
+      {AvatarOverlays}
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+  },
+  avatarOverlay: {
+    position: 'absolute',
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  avatarText: {
+    fontSize: 14,
+  },
+});
