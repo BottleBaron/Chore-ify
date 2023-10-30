78d3379daea35b23e74005ad8e3e90d275c34519
Graveknee <simoncarlstein@live.se>
2023-10-30 14:41:54 +0100

    Merge conflict resolved within chorelist

--- src/screens/ChoreListScreen/ChoreListScreen.tsx
+++ src/screens/ChoreListScreen/ChoreListScreen.tsx
@@ -5,45 +5,6 @@ import { useFocusEffect } from '@react-navigation/native';
 import * as React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 // eslint-disable-next-line import/no-cycle
-<<<<<<< HEAD
-import { Button } from 'react-native-paper';
-import { mockChores } from '../../../assets/Data/MockData';
-import { useAppTheme } from '../../contexts/ThemeContext';
-import { addChore } from '../../redux/slices/choreSlice';
-import { fetchHouseholds } from '../../redux/slices/householdSlice';
-import { useAppDispatch, useAppSelector } from '../../redux/store';
-import BottomButtons from './BottomButtonsComponent';
-import NoChoresPage from './NoChoresPage';
-
-// export type ChoreStackParamList = {
-// 	ChoreIndex: undefined;
-// 	Chore: undefined;
-// };
-
-export default function ChoreListScreen({ navigation }: any) {
-  // TODO: Replace placeholder id with actual id
-  const dispatch = useAppDispatch();
-  console.log('bingus');
-  const activeHouseHoldId = useAppSelector(
-    (state) => state.household.activeHouseHoldId,
-  );
-
-  const mockedChore = {
-    id: '1',
-    householdId: activeHouseHoldId,
-    title: 'Diska',
-    description: 'Diska och torka all smutsig disk i köket',
-    dayinterval: 2,
-    effortNumber: 2,
-  };
-
-  const handleAddChore = async () => {
-    await dispatch(fetchHouseholds());
-
-    const action = await dispatch(addChore(mockedChore));
-    console.log(action);
-  };
-=======
 import { mockChores } from '@src/assets/Data/MockData';
 // import { useAppTheme } from '@src/contexts/ThemeContext';
 import { useAppTheme } from '@src/contexts/ThemeContext';
@@ -54,7 +15,6 @@ import BottomButtons from './BottomButtonsComponent';
 import NoChoresPage from './NoChoresPage';
 
 type Props = HouseholdDashboardTabScreenProps<'ChoreList'>;
->>>>>>> 8fa3560c45146bdd0cf4a25b33044de83646a7ab
 
   const theme = useAppTheme();
   const dispatch = useAppDispatch();
